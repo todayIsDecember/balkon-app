6568197cafcc5d458fd6d5b2410559ae847bcac2
todayIsDecember <timkivit@gmail.com>
2024-02-12 23:11:12 +0200

    init app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,47 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  // experimental: {
+  //   appDir: true,
+  //   typedRoutes: true,
+  //   // scrollRestoration: true,
+  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost:3001",
+      },
+    ],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "preset-default",
+                  params: {
+                    overrides: {
+                      cleanupIds: false,
+                      removeViewBox: false,
+                    },
+                  },
+                },
+                "removeXMLNS",
+              ],
+            },
+          },
+        },
+      ],
+    });
 
-export default nextConfig;
+    return config;
+  },
+};
+
+module.exports = nextConfig;
