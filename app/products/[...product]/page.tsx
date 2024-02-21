d1947801ee970cde97cdc085827076e81c29abd3
todayIsDecember <timkivit@gmail.com>
2024-02-21 04:15:13 +0200

    fix bugs productpage 3

--- app/products/[...product]/page.tsx
+++ app/products/[...product]/page.tsx
@@ -1,10 +1,11 @@
 import { getProduct } from "@/app/api/getProduct"
 import { ProductPage, Wrapper } from "@/app/components"
+import styles from './page.module.css'
 
 export default async function Product({params}: {params: {product: number}}) {
   const product = await getProduct(params.product)
   return (
-    <Wrapper>
+    <Wrapper className={styles.wrapper}>
       <ProductPage product={product}/>
     </Wrapper>
   )
